78058c32efd1782fcae1eff7a45b480391d5708c
Shourya <mittalshourya97@gmail.com>
2019-01-12 18:44:02 +0530

    Modified main.js

--- server/connection.js
+++ server/connection.js
@@ -5,8 +5,22 @@ mongooose.connect('mongoose://localhost/hack_it');
 
 const schema = {
     hospitals: mongoose.model('hospitals', new mongoose({
-        h_name: String,
-        h_
+        hospital_name: String,
+        latitude : Number,
+        longitude: Number,
+        beds: Number,
+        location : String,
+        address : String,
+        state: String,
+        pincode : Number,
+        telephone : Number,
+        ambulance_ph_no : Number,
+        bloodbank_ph_no : Number,
+        website : String,
+        specialities : [String],
+        no_of_doctors : Number,
+        no_of_private_wards : Number,
+        no_of_beds_eco_weak: Number,
     }))
 }
 
